45c2e50f6ac7b3534de7bd47cdacaa7667da734a
Tom Morelly <tom-morelly@gmx.de>
2021-08-10 09:00:08 +0200

    various improvements

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ read -rp "GitHub Username: " user
 read -rp "Projectname: " projectname
 
 git clone git@github.com:FalcoSuessgott/golang-cli-template.git "$projectname"
-cd $projectname
+cd "$projectname"
 rm -rf .git
 find . -type f -exec sed -i "s/FalcoSuessgott\/golang-cli-template/$user\/$projectname/g" {} +
 git init
 git add .
 git commit -m "initial commit"
 git remote add origin "git@github.com:$user/$projectname.git"
+
+echo "template successfully installed."
